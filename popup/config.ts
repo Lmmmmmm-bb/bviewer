f351e04e99b83e26b8a136e0c0db022859a1164f
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-07-01 11:12:35 +0800

    feat: fetch video preview

---
+++ popup/config.ts
@@ -0,0 +1,3 @@
+export const matchBvidReg = /bilibili.com\/video\/(?<bvid>[A-Za-z0-9]+)/;
+
+export const B_API_VIDEO_INFO = 'https://api.bilibili.com/x/web-interface/view';
--- popup/index.tsx
+++ popup/index.tsx
@@ -1,20 +1,47 @@
 import { FC, useState } from 'react';
+import type { IBVideoInfoQuery } from '~types';
+import { B_API_VIDEO_INFO, matchBvidReg } from './config';
+import './index.css';
+import { concatUrlQuery } from './utils';
 
 const Popup: FC = () => {
-  const [data, setData] = useState('');
+  const [previewSrc, setPreviewSrc] = useState('');
+
+  const fetchVideoInfo = async (bvid: string) => {
+    const url = concatUrlQuery<IBVideoInfoQuery>(B_API_VIDEO_INFO, { bvid });
+    const response = await fetch(url);
+    const { data } = await response.json();
+    setPreviewSrc(data.pic);
+  };
+
+  const handleClick = async () => {
+    const [current] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    });
+
+    const { groups } = current.url.match(matchBvidReg);
+    if (groups) {
+      const { bvid } = groups;
+      !previewSrc && fetchVideoInfo(bvid);
+    }
+  };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 16
-      }}
-    >
-      <h1>
-        Welcome to your <a href='https://www.plasmo.com'>Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
+    <div className='wrapper'>
+      {previewSrc && (
+        <a
+          style={{ marginBottom: 16 }}
+          target='_blank'
+          title='download preview'
+          href={previewSrc}
+        >
+          <img className='preview' src={previewSrc} alt='video preview' />
+        </a>
+      )}
+      <button className='up-btn' onClick={handleClick}>
+        Video Preview
+      </button>
     </div>
   );
 };
