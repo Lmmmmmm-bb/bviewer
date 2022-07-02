21b53fa3030a03d7d533c4992b0dc870afddf704
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-07-02 19:55:19 +0800

    feat: support react router

--- popup/index.tsx
+++ popup/index.tsx
@@ -1,13 +1,21 @@
-import type { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { MemoryRouter, useNavigate } from 'react-router-dom';
 import { useFetchType } from '~hooks';
-import BiliBili from '~layouts/bilibili';
-import Follow from '~layouts/follow';
+import { Router } from '~layouts';
 import './index.scss';
 
 const Popup: FC = () => {
+  const navigator = useNavigate();
   const fetchType = useFetchType();
 
-  return <div>{fetchType === 'unusable' ? <Follow /> : <BiliBili />}</div>;
+  useEffect(() => {}, []);
+
+  // return <div>{fetchType === 'unusable' ? <Follow /> : <BiliBili />}</div>;
+  return (
+    <MemoryRouter>
+      <Router />
+    </MemoryRouter>
+  );
 };
 
 export default Popup;
