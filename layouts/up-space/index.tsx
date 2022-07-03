986f06e3bd1436eaf03f29239f05ac93f0113f92
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-07-04 00:16:43 +0800

    feat: support up video pagination

--- layouts/up-space/index.tsx
+++ layouts/up-space/index.tsx
@@ -1,7 +1,8 @@
 import { FC, useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
-import UpSpaceNavigate from '~components/up-space-navigate';
 import VideoCard from '~components/video-card';
+import UpSpaceNavigate from '~components/up-space-navigate';
+import UpSpacePagination from '~components/up-space-pagination';
 import type { IBSpaceQuery, ISpaceVideo, IUpInfo } from '~types';
 import { fetchBSpaceVideo } from './fetch';
 import styles from './index.module.scss';
@@ -9,11 +10,21 @@ import styles from './index.module.scss';
 const UpSpace: FC = () => {
   const { state } = useLocation();
   const { uid } = useParams<{ uid: string }>();
+  const [totalVideo, setTotalVideo] = useState(0);
   const [videoList, setVideoList] = useState<ISpaceVideo[]>([]);
 
   const fetchVideoList = async (query: Omit<IBSpaceQuery, 'mid'>) => {
-    const { vlist } = await fetchBSpaceVideo({ mid: uid, ...query });
+    const {
+      list: { vlist },
+      page: { count }
+    } = await fetchBSpaceVideo({ mid: uid, ...query });
     setVideoList(vlist);
+    setTotalVideo(count);
+  };
+
+  const handlePageClick = (page: number) => {
+    fetchVideoList({ pn: page });
+    scrollTo(0, 0);
   };
 
   useEffect(() => {
@@ -28,10 +39,15 @@ const UpSpace: FC = () => {
       ) : (
         <div className={styles.videoWrapper}>
           {videoList.map((video) => (
-            <VideoCard video={video} />
+            <VideoCard key={video.bvid} video={video} />
           ))}
         </div>
       )}
+      <UpSpacePagination
+        total={totalVideo}
+        onPrevClick={handlePageClick}
+        onNextClick={handlePageClick}
+      />
     </div>
   );
 };
