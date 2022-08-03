59bcd550b4eefa6e9edd9af64cd9405685a25b3c
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-08-03 23:59:20 +0800

    feat: error boundary wrapper

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,11 +1,18 @@
-import type { FC } from 'react';
+import type { FC, PropsWithChildren } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import ErrorBoundary from '~components/error-boundary';
 import Follow from './follow';
 import UpSpace from './up-space';
 
+const WrapperErrorBoundary = (FunctionComponent: FC) => (
+  <ErrorBoundary>
+    <FunctionComponent />
+  </ErrorBoundary>
+);
+
 export const Router: FC = () => (
   <Routes>
-    <Route path='/' element={<Follow />} />
-    <Route path='/up/:uid' element={<UpSpace />} />
+    <Route path='/' element={WrapperErrorBoundary(Follow)} />
+    <Route path='/up/:uid' element={WrapperErrorBoundary(UpSpace)} />
   </Routes>
 );
